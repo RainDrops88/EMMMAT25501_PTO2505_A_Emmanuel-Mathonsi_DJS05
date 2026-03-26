a616fb1d1331bc7a737cc9eaa620305e60cf7b83
Emmanuel <07942jerry@gmail.com>
2026-03-26 20:53:45 +0200

    creating new header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,17 @@
-import styles from "./Header.module.css";
+import { useContext } from "react";
+import { PodcastContext } from "../context/PodcastContext";
+
 export default function Header() {
+  const { search, setSearch } = useContext(PodcastContext);
+
   return (
-    <header className={styles.appHeader}>
+    <header className="app-header">
       <h1>🎙️ Podcast App</h1>
+      <div className="search">
+      <input className="search-bar" type="text" placeholder="Search podcasts..." value={search} onChange={(e) => setSearch(e.target.value)} />
+      
+      </div>
     </header>
+
   );
 }
