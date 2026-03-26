6f56c072ec8f3c2201daa70c3899ae861658a605
Emmanuel <07942jerry@gmail.com>
2026-03-26 20:54:12 +0200

    adding filter component

---
+++ src/components/Filter.jsx
@@ -0,0 +1,35 @@
+import { useContext } from "react";
+import { PodcastContext } from "../context/PodcastContext";
+
+export default function Filter() {
+    const { filter, setFilter, sort, setSort, genres } = useContext(PodcastContext);
+    return (
+        <div className="filters">
+            <label className="filter">Filter by:</label>
+            <select 
+                name="all-genres"
+                id="genre-list"
+                value={filter}
+                onChange={(e) => setFilter(e.target.value)}
+                >
+                <option value="all-genres" defaultValue>All genres</option>
+                {genres.map((genre) => (
+                    <option key={genre.id} value={genre.id}>{genre.title}</option>
+                ))}
+            </select>
+            <label className="sort">Sort by:</label>
+            <select
+                name="sort-by"
+                id="sort-by"
+                value={sort}
+                onChange={(e) => setSort(e.target.value)}
+            >
+                <option value="default">Default</option>
+                <option value="newest">Newest</option>
+                <option value="oldest">Oldest</option>
+                <option value="title_a-z">A-Z</option>
+                <option value="title_z-a">Z-A</option>
+            </select>
+        </div>
+    );
+}
