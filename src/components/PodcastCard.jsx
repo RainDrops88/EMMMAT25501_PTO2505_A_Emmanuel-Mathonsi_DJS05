a1f9efcddfd189782353d0b7f32cf1b3412cfcf5
Emmanuel <07942jerry@gmail.com>
2026-03-26 20:56:44 +0200

    creating podcast card

--- src/components/PodcastCard.jsx
+++ src/components/PodcastCard.jsx
@@ -1,6 +1,6 @@
 import { formatDate } from "../utils/formatDate";
-import styles from "./PodcastCard.module.css";
 import { genre } from "../utils/genres";
+import { Link } from "react-router-dom";
 
 /**
  * Renders a single podcast preview card with image, title, number of seasons,
@@ -18,25 +18,20 @@ import { genre } from "../utils/genres";
  * @returns {JSX.Element} The rendered podcast card component.
  */
 export default function PodcastCard({ podcast, genres }) {
-  const genreSpans = podcast.genres.map((id) => {
-    const match = genres.find((genre) => genre.id === id);
-    return (
-      <span key={id} className={styles.tag}>
-        {match ? match.title : `Unknown (${id})`}
-      </span>
-    );
-  });
+ 
 
   return (
-    <div className={styles.card}>
+   <Link key={podcast.id} to={`/detailed-card/${podcast.id}`}>
+    <div className="card">
       <img src={podcast.image} alt={podcast.title} />
 
       <h3>{podcast.title}</h3>
-      <p className={styles.seasons}>{podcast.seasons} seasons</p>
-      <div className={styles.tags}>{genreSpans}</div>
-      <p className={styles.updatedText}>
-        Updated {formatDate(podcast.updated)}
-      </p>
+      <p className="seasons">{podcast.seasons} seasons</p>
+      <div className="tags">{genre.getGenreNames(podcast.genres, genres).map((title, index) => (
+        <span key={index} className="tag">{title}</span>
+      ))}</div>
+      <p className="updated-text">Updated {formatDate(podcast.updated)}</p>
     </div>
+   </Link>
   );
 }
