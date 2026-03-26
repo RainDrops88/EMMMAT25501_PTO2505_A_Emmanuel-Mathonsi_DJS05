c567618002324e343b47d3287d200725bb6c32f5
Emmanuel <07942jerry@gmail.com>
2026-03-26 20:57:57 +0200

    adding routers on app components

--- src/App.jsx
+++ src/App.jsx
@@ -1,62 +1,71 @@
 import { useEffect, useState } from "react";
-import { PodcastProvider } from "./context/PodcastContext";
-import { fetchPodcasts } from "./api/fetchPodcasts";
-import { genres } from "./data";
-import Header from "./components/Header";
-import SearchBar from "./components/SearchBar";
-import SortSelect from "./components/SortSelect";
-import GenreFilter from "./components/GenreFilter";
 import PodcastGrid from "./components/PodcastGrid";
-import Pagination from "./components/Pagination";
-import styles from "./App.module.css";
+import { fetchPodcasts } from "./api/fetchPodcasts";
+import Header from "./components/Header";
+import Filter from "./components/Filter";
+import {PodcastProvider} from "./context/PodcastContext"
+import Pagination  from "./components/Pagination"
+import { Routes, Route } from 'react-router-dom';
+import DetailedCard from "./components/DetailedCard";
 
 /**
- * Root component of the Podcast Explorer app.
- * Handles data fetching and layout composition.
+ * App - The root component of the Podcast Explorer application. It handles:
+ * - Fetching podcast data from a remote API
+ * - Managing search, sort and filter states
+ * - Managing loading and error states
+ * - Rendering the podcast grid once data is successfully fetched
+ * - Displaying a header and fallback UI during loading or error
+ * @returns {JSX.Element} The rendered application interface
  */
 export default function App() {
   const [podcasts, setPodcasts] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
+  
   useEffect(() => {
     fetchPodcasts(setPodcasts, setError, setLoading);
   }, []);
 
   return (
     <>
+      <PodcastProvider initialPodcast={podcasts}>
       <Header />
+      
+      
+      <main>
+        {loading && (
+          <div className="message-container">
+            <div className="spinner"></div>
+            <p>Loading podcasts...</p>
+          </div>
+        )}
 
-      <PodcastProvider initialPodcasts={podcasts}>
-        <main className={styles.main}>
-          <section className={styles.controls}>
-            <SearchBar />
-            <GenreFilter genres={genres} />
-            <SortSelect />
-          </section>
-
-          {loading && (
-            <div className={styles.messageContainer}>
-              <div className={styles.spinner}></div>
-              <p>Loading podcasts...</p>
+        {error && (
+          <div className="message-container">
+            <div className="error">
+              Error occurred while trying fetching podcasts: {error}
             </div>
-          )}
+          </div>
+        )}
 
-          {error && (
-            <div className={styles.message}>
-              <div className={styles.error}>
-                Error occurred while fetching podcasts: {error}
-              </div>
-            </div>
-          )}
-
-          {!loading && !error && (
-            <>
-              <PodcastGrid genres={genres} />
-              <Pagination />
-            </>
-          )}
-        </main>
+        {!loading && !error && (
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <>
+                  <Filter />
+                  <PodcastGrid />
+                  <Pagination />
+                </>
+              }
+            />
+            <Route path="/detailed-card/:id" element={<DetailedCard />} />
+          </Routes>
+        )}
+      </main>
+      
       </PodcastProvider>
     </>
   );
